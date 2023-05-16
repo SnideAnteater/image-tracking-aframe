96883ae29d4d8d29b399aa72e9624e9e648f5472
Akmal Yassin <totsmcgeee@gmail.com>
2023-05-16 12:07:36 +0800

    added Detect.js to detect browser being used

--- main.js
+++ main.js
@@ -1,13 +1,16 @@
-import './style.css'
+import "./style.css";
+
+const ua = window.detect.parse(navigator.userAgent);
+console.log(ua.browser);
 
 AFRAME.registerComponent("spawn-model", {
-  init: function() {
+  init: function () {
     const marker = this.el.object3D;
     const sceneEl = document.querySelector("a-scene");
     const modelEl = document.querySelector("#model");
     const model2El = document.querySelector("#model2");
 
-    marker.addEventListener("markerFound", function() {
+    marker.addEventListener("markerFound", function () {
       alert("Hello! I am an alert box!!");
       if (this.id === "model") {
         sceneEl.appendChild(modelEl.cloneNode(true));
@@ -16,13 +19,13 @@ AFRAME.registerComponent("spawn-model", {
       }
     });
 
-    marker.addEventListener("markerLost", function() {
+    marker.addEventListener("markerLost", function () {
       const spawnedModelEls = document.querySelectorAll(".spawned-model");
-      spawnedModelEls.forEach(function(spawnedModelEl) {
+      spawnedModelEls.forEach(function (spawnedModelEl) {
         spawnedModelEl.parentNode.removeChild(spawnedModelEl);
       });
     });
-  }
+  },
 });
 
 const marker1 = document.querySelector("#model");
